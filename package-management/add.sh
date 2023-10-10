0ecabff7df5737fe8b8b2ee9c42b73b672dbbac4
Kamil Neczaj <kneczaj@gmail.com>
2023-10-10 12:59:06 +0200

    Change commit messages

--- package-management/add.sh
+++ package-management/add.sh
@@ -3,6 +3,6 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 YARN_ARGS="${@:1}"
 LABEL=${CONTEXT-""}
-COMMIT_MESSAGE="[PackageAdd]${LABEL} ${YARN_ARGS}"
+COMMIT_MESSAGE="${LABEL}[PackageAdd] ${YARN_ARGS}"
 
 ${SCRIPT_DIR}/generate_commit.sh "${COMMIT_MESSAGE}" yarn add ${YARN_ARGS}
--- package-management/remove.sh
+++ package-management/remove.sh
@@ -3,6 +3,6 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 YARN_ARGS="${@:1}"
 LABEL=${CONTEXT-""}
-COMMIT_MESSAGE="[PackageRemove]${LABEL} ${YARN_ARGS}"
+COMMIT_MESSAGE="${LABEL}[PackageRemove] ${YARN_ARGS}"
 
 ${SCRIPT_DIR}/generate_commit.sh "${COMMIT_MESSAGE}" yarn remove ${YARN_ARGS}
